b2fa9e6506c5424df698d2410c51fc5986a78e81
Kirill Vercetti <office@kyzima-spb.com>
2018-09-16 20:36:18 +0300

    Add mongoengine, but not testing

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='authlib-database',
+    version='0.1.0',
+    packages=['authlib_database', 'authlib_database.oauth2'],
+    url='https://github.com/kyzima-spb/authlib-database',
+    license='GNU AGPLv3+',
+    author='Kirill Vercetti',
+    author_email='office@kyzima-spb.com',
+    description='',
+    install_requires=[
+        'Authlib'
+    ]
+)
