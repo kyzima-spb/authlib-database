3d4f3e4326e7ec65621c8232fe32eb213e5a715d
Kirill Vercetti <office@kyzima-spb.com>
2019-02-22 02:50:39 +0300

    Update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='authlib-database',
-    version='0.1.0',
+    version='0.2.0',
     packages=['authlib_database', 'authlib_database.oauth2'],
     url='https://github.com/kyzima-spb/authlib-database',
     license='GNU AGPLv3+',
